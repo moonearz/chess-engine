c5724c08dbce1ed6e156b7e22d1f85ff19a4f2db
moonearz <munirbenjemaa@gmail.com>
2024-02-12 18:37:17 -0500

    assert

--- defs.h
+++ defs.h
@@ -2,6 +2,21 @@
 #ifndef DEFS_H
 #define DEFS_H
 
+#include "stdlib.h"
+
+#ifndef DEBUG
+#define ASSERT(n)
+#else
+#define ASSERT(n) \
+if(!(n)) {\
+printf("%s - Failed", #n); \
+printf("On %s ",__DATE__); \
+printf("At %s ",__TIME__); \
+printf("In File %s ",__FILE__); \
+printf("At Line %d\n ",__LINE__); \
+exit(1);}
+#endif
+
 typedef unsigned long long U64;
 
 #define NAME "methane"
@@ -56,6 +71,8 @@ typedef struct {
     int minPces[3];
 
     S_UNDO history[MAXGAMEMOVES];
+
+    int pList[13][10];
 } S_BOARD;
 
 /*  MACROS  */
