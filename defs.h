d53a339a661d46b664038780231e145392197555
moonearz <munirbenjemaa@gmail.com>
2024-02-18 17:47:30 -0500

    move bit-macros

--- defs.h
+++ defs.h
@@ -89,13 +89,26 @@ typedef struct {
 
 /* GAME MOVE */
 /*
-0000 0000 0000 0000 0000 0111 1111 ->From
-0000 0000 0000 0011 1111 1000 0000 ->To
-0000 0000 0011 1100 0000 0000 0000 ->Captured piece
-0000 0000 0100 0000 0000 0000 0000 ->EP capture?
-0000 0111 1000 0000 0000 0000 0000 ->PawnSt/Promotion piece
-0000 1000 0000 0000 0000 0000 0000 ->Castle
+0000 0000 0000 0000 0000 0111 1111 ->From 0x3F
+0000 0000 0000 0011 1111 1000 0000 ->To >> 7 0x3F
+0000 0000 0011 1100 0000 0000 0000 ->Captured piece >> 14 0xF
+0000 0000 0100 0000 0000 0000 0000 ->EP capture? 0x40000
+0000 0000 1000 0000 0000 0000 0000 ->PawnSt 0x80000
+0000 1111 0000 0000 0000 0000 0000 ->Promotion piece >> 20 0xF
+0001 0000 0000 0000 0000 0000 0000 ->Castle 0x1000000
 */
+
+#define FROMSQ(m) ((m) & 0x7F)
+#define TOSQ(m) (((m) >> 7) & 0x7F)
+#define CAPTURED(m) (((m) >> 14) & 0xF)
+#define PROMOTED(m) (((m) >> 20) & 0xF)
+
+#define MFEP 0x40000
+#define MFPS 0x80000
+//oh dear is this terrible terminology or what
+#define MFCA 0x1000000
+#define MFCAP 0x7C000
+#define MFPRO 0xF00000
 
 /*  MACROS  */
 #define FR2SQ(f, r) ( (21 + (f)) + ( (r) * 10) )
