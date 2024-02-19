75bce900b1596d80e399df2a0d1c3988e231b9de
moonearz <munirbenjemaa@gmail.com>
2024-02-19 18:41:20 -0500

    black pawn moves

--- engine.c
+++ engine.c
@@ -6,7 +6,8 @@
 #define FEN2 "rnbqkbnr/pp1ppppp/8/2p5/4P3/8/PPPP1PPP/RNBQKBNR w KQkq c6 0 2"
 #define FEN3 "rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2"
 #define FEN4 "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1"
-#define TEST_PAWN_MOVES "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR w KQkq e6 0 1"
+//if white to move add e6 ep square, w 
+#define TEST_PAWN_MOVES "rnbqkb1r/pp1p1pPp/8/2p1pP2/1P1P4/3P3P/P1P1P3/RNBQKBNR b KQkq - 0 1"
 
 int main() {
     AllInit();
--- movegen.c
+++ movegen.c
@@ -34,12 +34,36 @@ void AddWPCapMove(const S_BOARD *pos, const int from, const int to, const int ca
     }
 }
 
+void AddBPCapMove(const S_BOARD *pos, const int from, const int to, const int cap, S_MOVELIST *list) {
+    if(RanksBrd[from] == RANK_2) {
+        AddCaptureMove(pos, MOVE(from,to,cap,bQ,0), list);
+        AddCaptureMove(pos, MOVE(from,to,cap,bR,0), list);
+        AddCaptureMove(pos, MOVE(from,to,cap,bB,0), list);
+        AddCaptureMove(pos, MOVE(from,to,cap,bN,0), list);
+    }
+    else {
+        AddCaptureMove(pos, MOVE(from,to,cap,EMPTY,0), list);
+    }
+}
+
 void AddWPQuietMove(const S_BOARD *pos, const int from, const int to, S_MOVELIST *list) {
     if(RanksBrd[from] == RANK_7) {
         AddQuietMove(pos, MOVE(from,to,EMPTY,wQ,0), list);
         AddQuietMove(pos, MOVE(from,to,EMPTY,wR,0), list);
         AddQuietMove(pos, MOVE(from,to,EMPTY,wB,0), list);
         AddQuietMove(pos, MOVE(from,to,EMPTY,wN,0), list);
+    }
+    else {
+        AddQuietMove(pos, MOVE(from,to,EMPTY,EMPTY,0), list);
+    }
+}
+
+void AddBPQuietMove(const S_BOARD *pos, const int from, const int to, S_MOVELIST *list) {
+    if(RanksBrd[from] == RANK_2) {
+        AddQuietMove(pos, MOVE(from,to,EMPTY,bQ,0), list);
+        AddQuietMove(pos, MOVE(from,to,EMPTY,bR,0), list);
+        AddQuietMove(pos, MOVE(from,to,EMPTY,bB,0), list);
+        AddQuietMove(pos, MOVE(from,to,EMPTY,bN,0), list);
     }
     else {
         AddQuietMove(pos, MOVE(from,to,EMPTY,EMPTY,0), list);
@@ -86,6 +110,32 @@ void GenerateAllMoves(const S_BOARD *pos, S_MOVELIST *list) {
         }
     }
     else {
+        for(pceNum = 0; pceNum < pos->pceNum[bP]; ++pceNum) {
+            sq = pos->pList[bP][pceNum];
+            ASSERT(SqOnBoard(sq));
 
+            if(pos->pieces[sq - 10] == EMPTY) {
+                AddBPQuietMove(pos, sq, sq - 10, list);
+                if(RanksBrd[sq] == RANK_7 && pos->pieces[sq - 20] == EMPTY) {
+                    AddQuietMove(pos, MOVE(sq, (sq - 20), EMPTY, EMPTY, MFPS), list);
+                }
+            }
+
+            if(!SQOFFBOARD(sq - 9) && PieceCol[pos->pieces[sq - 9]] == WHITE) {
+                AddBPCapMove(pos, sq, sq - 9, pos->pieces[sq - 9], list);
+            }
+
+            if(!SQOFFBOARD(sq - 11) && PieceCol[pos->pieces[sq - 11]] == WHITE) {
+                AddBPCapMove(pos, sq, sq - 11, pos->pieces[sq - 11], list);
+            }
+
+            if(sq - 9 == pos->enPas) {
+                AddCaptureMove(pos, MOVE(sq, sq - 9, EMPTY, EMPTY, MFEP), list);
+            }
+
+            if(sq - 11 == pos->enPas) {
+                AddCaptureMove(pos, MOVE(sq, sq - 11, EMPTY, EMPTY, MFEP), list);
+            }
+        }
     }
 }
