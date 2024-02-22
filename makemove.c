c7140d0c0fe1f59aa88f96b721a6b5063641f11c
moonearz <munirbenjemaa@gmail.com>
2024-02-22 16:43:08 -0500

    clearing pieces

--- makemove.c
+++ makemove.c
@@ -1,3 +1,6 @@
+#include "defs.h"
+#include "stdio.h"
+
 #define HASH_PCE(pce, sq) (pos->posKey ^= (PieceKeys[(pce)][(sq)]))
 #define HASH_CA (pos->posKey ^= (CastleKeys[(pos->castlePerm)]))
 #define HASH_SIDE (pos->posKey ^= sideKey)
@@ -17,3 +20,46 @@ const int CastlePerm[120] = {
     15,15,15,15,15,15,15,15,15,15,
     15,15,15,15,15,15,15,15,15,15,
 };
+
+static void ClearPiece(const int sq, S_BOARD *pos) {
+    ASSERT(SqOnBoard(sq));
+
+    int pce = pos->pieces[sq];
+
+    ASSERT(PieceValid(pce));
+
+    int col = PieceCol[pce];
+    int index = 0;
+    int t_pceNum = -1;
+
+    HASH_PCE(pce, sq);
+
+    pos->pieces[sq] = EMPTY;
+    pos->material[col] -= PieceVal[pce];
+
+    if(Piece[pce]) {
+        pos->Pces[col]--;
+        if(PieceMaj[pce]) {
+            pos->majPces[col]--;
+        }
+        else {
+            pos->minPces[col]--;
+        }
+    }
+    else {
+        CLRBIT(pos->pawns[col], SQ64(sq));
+        CLRBIT(pos->pawns[BOTH], SQ64(sq));
+    }
+
+    for(index = 0; index < pos->pceNum[pce]; ++index) {
+        if(pos->pList[pce][index] == sq) {
+            t_pceNum = index;
+            break;
+        }
+    }
+
+    ASSERT(t_pceNum != -1);
+
+    pos->pceNum[pce]--;
+    pos->pList[pce][t_pceNum] = pos->pList[pce][pos->pceNum[pce]];
+}
