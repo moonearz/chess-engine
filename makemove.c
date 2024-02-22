a82574de043de30fefe3196c63a07968b7eafec3
moonearz <munirbenjemaa@gmail.com>
2024-02-22 18:29:56 -0500

    make move function

--- makemove.c
+++ makemove.c
@@ -3,7 +3,7 @@
 
 #define HASH_PCE(pce, sq) (pos->posKey ^= (PieceKeys[(pce)][(sq)]))
 #define HASH_CA (pos->posKey ^= (CastleKeys[(pos->castlePerm)]))
-#define HASH_SIDE (pos->posKey ^= sideKey)
+#define HASH_SIDE (pos->posKey ^= SideKey)
 #define HASH_EP (pos->posKey ^= (PieceKeys[EMPTY][(pos->enPas)]))
 
 const int CastlePerm[120] = { 
@@ -120,4 +120,112 @@ static void MovePiece(const int from, const int to, S_BOARD *pos) {
             break;
         }
     }
+}
+
+int MakeMove(S_BOARD *pos, int move) {
+    ASSERT(CheckBoard(pos));
+
+    int from = FROMSQ(move);
+    int to = TOSQ(move);
+    int side = pos->side;
+
+    ASSERT(SqOnBoard(from));
+    ASSERT(SqOnBoard(to));
+    ASSERT(SideValid(side));
+    ASSERT(PieceValid(pos->pieces[from]));
+
+    pos->history[pos->hisPly].posKey = pos->posKey;
+
+    if(move & MFEP) {
+        if(side == WHITE) {
+            ClearPiece(to - 10, pos);
+        }
+        else {
+            ClearPiece(to + 10, pos);
+        }
+    }
+    else if(move & MFCA) {
+        switch(to) {
+            case C1:
+                MovePiece(A1, D1, pos);
+            break;
+            case C8:
+                MovePiece(A8, D8, pos);
+            break;
+            case G1:
+                MovePiece(H1, F1, pos);
+            break;
+            case G8:
+                MovePiece(H8, F8, pos);
+            break;
+            default: ASSERT(FALSE); break;
+        }
+    }
+
+    if(pos->enPas != NO_SQ) HASH_EP;
+    HASH_CA;
+
+    pos->history[pos->hisPly].move = move;
+    pos->history[pos->hisPly].fiftyMove = pos->fiftyMove;
+    pos->history[pos->hisPly].enPas = pos->enPas;
+    pos->history[pos->hisPly].castlePerm = pos->castlePerm;
+
+    pos->castlePerm &= CastlePerm[from];
+    pos->castlePerm &= CastlePerm[to];
+    pos->enPas = NO_SQ;
+
+    HASH_CA;
+
+    int captured = CAPTURED(move);
+    pos->fiftyMove++;
+
+    if(captured != EMPTY) {
+        ASSERT(PieceValid(captured));
+        ClearPiece(to, pos);
+        pos->fiftyMove = 0;
+    }
+
+    pos->hisPly++;
+    pos->ply++;
+
+    if(PiecePawn[pos->pieces[from]]) {
+        pos->fiftyMove = 0;
+        if(move & MFPS) {
+            if(side == WHITE) {
+                pos->enPas = from + 10;
+                ASSERT(RanksBrd[pos->enPas] == RANK_3);
+            }
+            else {
+                pos->enPas = from - 10;
+                ASSERT(RanksBrd[pos->enPas] == RANK_6);
+            }
+            HASH_EP;
+        }
+    }
+
+    MovePiece(from, to, pos);
+
+    int prPce = PROMOTED(move);
+    if(prPce != EMPTY) {
+        ASSERT(PieceValid(prPce) && !PiecePawn[prPce]);
+        ClearPiece(to, pos);
+        AddPiece(to, pos, prPce);
+    }
+    /*not neccesary? 
+    if(PieceKing[pos->pieces[to]]) {
+        pos->KingSq[pos->side] = to;
+    }
+    */
+
+    pos->side ^= 1;
+    HASH_SIDE;
+
+    ASSERT(CheckBoard(pos));
+
+    if(SqAttacked(pos->KingSq[side], pos->side, pos)) {
+        //TakeMove(pos);
+        return FALSE;
+    }
+
+    return TRUE;
 }
