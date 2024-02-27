1816f8c607cc4ac68907bfa3401a47a2a7fb7e2c
moonearz <munirbenjemaa@gmail.com>
2024-02-27 17:21:19 -0500

    search preparation

--- search.c
+++ search.c
@@ -21,7 +21,27 @@ static int IsRepetition(const S_BOARD *pos) {
 }
 
 static void ClearForSearch(S_BOARD *pos, S_SEARCHINFO *info) {
+    int index = 0;
+    int index2 = 0;
 
+    for(index = 0; index  < 13; ++index) {
+        for(index2 = 0; index2 < BRD_SQ_NUM; ++index2) {
+            pos->searchHistory[index][index2] = 0;
+        }
+    }
+
+    for(index = 0; index  < 2; ++index) {
+        for(index2 = 0; index2 < MAXDEPTH; ++index2) {
+            pos->searchEnders[index][index2] = 0;
+        }
+    }
+
+    ClearPvTable(pos->PvTable);
+    pos->ply = 0;
+
+    info->starttime = GetTimeMs();
+    info->stopped = 0;
+    info->nodes = 0;
 }
 
 static int Quiescence(int alpha, int beta, S_BOARD *pos, S_SEARCHINFO *info) {
