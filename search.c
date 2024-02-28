bb387b62cd2f129c2d9e8b4de129044c77adeeca
moonearz <munirbenjemaa@gmail.com>
2024-02-28 17:39:58 -0500

    alpha-beta search

--- search.c
+++ search.c
@@ -49,7 +49,63 @@ static int Quiescence(int alpha, int beta, S_BOARD *pos, S_SEARCHINFO *info) {
 }
 
 static int AlphaBeta(int alpha, int beta, int depth, S_BOARD *pos, S_SEARCHINFO *info, int DoNull) {
-    return 0;
+    ASSERT(CheckBoard(pos));
+
+    if(depth == 0) {
+        info->nodes++;
+        return EvalPosition(pos);
+    }
+
+    info->nodes++;
+
+    if(IsRepetition(pos) || pos->fiftyMove >= 100) {
+        return 0;
+    }
+
+    if(pos->ply > MAXDEPTH - 1) {
+        return EvalPosition(pos);
+    }
+
+    S_MOVELIST list[1];
+    GenerateAllMoves(pos, list);
+    int MoveNum = 0;
+    int Legal = 0;
+    int OldAlpha = alpha;
+    int BestMove = NOMOVE;
+    int Score = -INFINITE;
+    
+    for(MoveNum = 0; MoveNum < list->count; ++MoveNum) {
+        if(!MakeMove(pos, list->moves[MoveNum].move)) {
+            continue;
+        }
+
+        Legal++;
+        Score = -AlphaBeta(-beta, -alpha, depth - 1, pos, info, TRUE);
+        TakeMove(pos);
+
+        if(Score > alpha) {
+            if(Score >= beta) {
+                return beta;
+            }
+            alpha = Score;
+            BestMove = list->moves[MoveNum].move;
+        }
+    }
+
+    if(Legal == 0) {
+        if(SqAttacked(pos->KingSq[pos->side], pos->side^1, pos)) {
+            return -INFINITE + pos->ply;
+        }
+        else {
+            return 0;
+        }
+    }
+
+    if(alpha != OldAlpha) {
+        StorePvMove(pos, BestMove);
+    }
+
+    return alpha;
 }
 
 void SearchPosition(S_BOARD *pos, S_SEARCHINFO *info) {
