7364fc21816e9c55e359da7f2cf6bf83faeb41ea
moonearz <munirbenjemaa@gmail.com>
2024-02-29 16:08:21 -0500

    king location eval

--- evaluate.c
+++ evaluate.c
@@ -74,11 +74,43 @@ const int KingO[64] = {
 	-70	,	-70	,	-70	,	-70	,	-70	,	-70	,	-70	,	-70		
 };
 
+// sjeng 11.2 with best play
+//8/6R1/2k5/6P1/8/8/4nP2/6K1 w - - 1 41 
+int MaterialDraw(const S_BOARD *pos) {
+
+	ASSERT(CheckBoard(pos));
+	
+    if (!pos->pceNum[wR] && !pos->pceNum[bR] && !pos->pceNum[wQ] && !pos->pceNum[bQ]) {
+	  if (!pos->pceNum[bB] && !pos->pceNum[wB]) {
+	      if (pos->pceNum[wN] < 3 && pos->pceNum[bN] < 3) {  return TRUE; }
+	  } else if (!pos->pceNum[wN] && !pos->pceNum[bN]) {
+	     if (abs(pos->pceNum[wB] - pos->pceNum[bB]) < 2) { return TRUE; }
+	  } else if ((pos->pceNum[wN] < 3 && !pos->pceNum[wB]) || (pos->pceNum[wB] == 1 && !pos->pceNum[wN])) {
+	    if ((pos->pceNum[bN] < 3 && !pos->pceNum[bB]) || (pos->pceNum[bB] == 1 && !pos->pceNum[bN]))  { return TRUE; }
+	  }
+	} else if (!pos->pceNum[wQ] && !pos->pceNum[bQ]) {
+        if (pos->pceNum[wR] == 1 && pos->pceNum[bR] == 1) {
+            if ((pos->pceNum[wN] + pos->pceNum[wB]) < 2 && (pos->pceNum[bN] + pos->pceNum[bB]) < 2)	{ return TRUE; }
+        } else if (pos->pceNum[wR] == 1 && !pos->pceNum[bR]) {
+            if ((pos->pceNum[wN] + pos->pceNum[wB] == 0) && (((pos->pceNum[bN] + pos->pceNum[bB]) == 1) || ((pos->pceNum[bN] + pos->pceNum[bB]) == 2))) { return TRUE; }
+        } else if (pos->pceNum[bR] == 1 && !pos->pceNum[wR]) {
+            if ((pos->pceNum[bN] + pos->pceNum[bB] == 0) && (((pos->pceNum[wN] + pos->pceNum[wB]) == 1) || ((pos->pceNum[wN] + pos->pceNum[wB]) == 2))) { return TRUE; }
+        }
+    }
+  return FALSE;
+}
+
+#define ENDGAME_MATERIAL (1 * PieceVal[wR] + 2 * PieceVal[wN] + 2 * PieceVal[wP] + PieceVal[wK])
+
 int EvalPosition(const S_BOARD *pos) {
     int pce;
     int pceNum;
     int sq;
     int score = pos->material[WHITE] - pos->material[BLACK];
+
+	if(MaterialDraw(pos)) {
+		return 0;
+	}
 
     pce = wP;
     for(pceNum = 0; pceNum < pos->pceNum[pce]; ++pceNum) {
@@ -194,6 +226,26 @@ int EvalPosition(const S_BOARD *pos) {
 			score -= QueenSemiOpenFile;
 		}
 	}		
+
+	pce = wK;
+	sq = pos->pList[pce][0];
+
+	if(pos->pceNum[bQ] == 0 || (pos->material[BLACK] <= ENDGAME_MATERIAL)) {
+		score += KingE[SQ64(sq)];
+	}
+	else {
+		score += KingO[SQ64(sq)];
+	}
+
+	pce = bK;
+	sq = pos->pList[pce][0];
+
+	if(pos->pceNum[wQ] == 0 || (pos->material[WHITE] <= ENDGAME_MATERIAL)) {
+		score -= KingE[MIRROR64(SQ64(sq))];
+	}
+	else {
+		score -= KingO[MIRROR64(SQ64(sq))];
+	}
 	
     if(pos->side == WHITE) {
         return score;
